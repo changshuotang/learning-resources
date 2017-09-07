05b25f3ebafdada856c476d0c2dde73145fc21c3
Rey Tang <tang.changrey@gmail.com>
2017-09-06 18:04:58 -0700

    more lols

--- interview_prep.java
+++ interview_prep.java
@@ -2004,7 +2004,52 @@ TreeNode returnCorrespondingCopyNode(TreeNode root, TreeNode copyRoot, TreeNode 
 	* You are allowed to use the +, -, * and / operations.
 	*/
 
+boolean isReachable(ArrayList<Integer> list, int target) {
+  if (list == null || list.size() == 0)
+    return false;
+ 
+  int i = 0;
+  int j = list.size() - 1;
+ 
+  ArrayList<Integer> results = getResults(list, i, j, target);
+ 
+  for (int num : results) {
+    if (num == target) {
+      return true;
+    }
+  }
+ 
+  return false;
+} 
 
+ArrayList<Integer> getResults(ArrayList<Integer> list,int left, int right, int target) {
+  ArrayList<Integer> result = new ArrayList<Integer>();
+ 
+  if (left > right) {
+    return result;
+  } else if (left == right) {
+    result.add(list.get(left));
+    return result;
+  }
+ 
+  for (int i = left; i < right; i++) {
+ 
+    ArrayList<Integer> result1 = getResults(list, left, i, target);
+    ArrayList<Integer> result2 = getResults(list, i + 1, right, target);
+ 
+    for (int x : result1) {
+      for (int y : result2) {
+        result.add(x + y);
+        result.add(x - y);
+        result.add(x * y);
+        if (y != 0)
+          result.add(x / y);
+      }
+    }
+  }
+ 
+  return result;
+}
 
 /**
   * We are given desired array target[] containing n elements. Compute and return the smallest possible number of the operations needed to change the array from all zeros to desired array.
